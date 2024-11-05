8d1034bc53eb5fda62d1e29c325d18144ddc5cf5
qiliahold <vn44h0i@delhaize.com>
2024-11-04 18:52:26 -0800

    pr8

--- carharrt_dlt/training/notebooks/TrainWithFeatureStore.py
+++ carharrt_dlt/training/notebooks/TrainWithFeatureStore.py
@@ -1,6 +1,6 @@
 # Databricks notebook source
 ##################################################################################
-# Model Training Notebook using Databricks Feature Store pr7
+# Model Training Notebook using Databricks Feature Store pr8
 #
 # This notebook shows an example of a Model Training pipeline using Databricks Feature Store tables.
 # It is configured and can be executed as the "Train" task in the model_training_job workflow defined under
