9b0bc7ce50755be4ef02e327d759e3c11d9f04df
qiliahold <vn44h0i@delhaize.com>
2024-11-06 14:53:47 -0800

    fix

--- carharrt_dlt/tests/dsg_test.py
+++ carharrt_dlt/tests/dsg_test.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 
-# with open("/dbfs/temp.txt", "w") as fout:
-#     fout.write("hi")
+with open("temp.txt", "w") as fout:
+    fout.write("hi")
 # The error occurs because writing directly to DBFS using the standard Python open function is not supported. Instead, you should use Databricks utilities to write to DBFS.
 
-dbutils.fs.put("/dbfs/temp.txt", "hi", overwrite=True)
+# dbutils.fs.put("/dbfs/temp.txt", "hi", overwrite=True)
